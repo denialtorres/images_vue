833c9633ca60f61cb4628c0446d7a5901889498f
Daniel Torres <denial.torres@gmail.com>
2021-12-27 13:14:53 -0600

    Authentication in Vue with OAuth2

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,14 +3,17 @@
     <a href="/" class="active item"> Image Storage </a>
 
     <div class="right menu">
-      <a class="ui item"> Log in </a>
+      <a href="#" class="ui item" @click="login"> Log in </a>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "AppHeader",
+  methods: mapActions(["login"]),
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import Vuex from "vuex";
 import Vue from "vue";
+import auth from "./modules/auth";
 
 // conect Vuex to Vue
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {},
+  modules: {
+    auth: auth,
+  },
 });
