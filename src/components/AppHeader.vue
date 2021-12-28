38f01cb246cec9d551d9800091cad956f3d91761
Daniel Torres <denial.torres@gmail.com>
2021-12-27 19:30:24 -0600

    Navigation with Vue Router

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="ui secondary pointing menu">
-    <a href="/" class="active item"> Image Storage </a>
-
+    <router-link to="/" class="active item"> Image Storage </router-link>
     <div class="right menu">
-      <a href="#" class="ui item" @click="login"> Log in </a>
+      <div v-if="isLoggedIn" class="horizontal">
+        <router-link to="/" class="item">Galleries</router-link>
+        <router-link to="/upload" class="item">Upload</router-link>
+        <a class="item" @click="logout">Logout</a>
+      </div>
+      <a v-else href="#" class="ui item" @click="login"> Log in </a>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "AppHeader",
-  methods: mapActions(["login"]),
+  computed: mapGetters(["isLoggedIn"]),
+  methods: mapActions(["login", "logout"]),
 };
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
+</style>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,13 +1,15 @@
 import api from "../../api/imgur";
+import qs from "qs";
+import { router } from "../../main";
 
 const state = {
-  token: null,
+  token: window.localStorage.getItem("imgur_token"),
 };
 
 const getters = {
   isLoggedIn: function (state) {
     // !! turn value into a boolean
-    !!state.token;
+    return !!state.token;
   },
 };
 
@@ -18,11 +20,18 @@ const actions = {
   logout: function ({ commit }) {
     commit("setToken", null);
   },
+  finalizeLogin: function ({ commit }, hash) {
+    const query = qs.parse(hash.replace("#", ""));
+    commit("setToken", query.access_token);
+    window.localStorage.setItem("imgur_token", query.access_token);
+    router.push("/");
+  },
 };
 
 const mutations = {
   setToken: function (state, token) {
     state.token = token;
+    window.localStorage.removeItem("imgur_token");
   },
 };
 
